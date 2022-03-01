abf9c4f2cc9f9374c20240eab121a6236209c743
Pratham <pratham7838@gmail.com>
2022-03-02 00:39:09 +0530

    Removed the React Router components.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import { useState } from 'react';
 import './App.css';
-import About from './components/About';
+// import About from './components/About';
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import React from 'react'
 import Alert from './components/Alert';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route,
+// } from "react-router-dom";
 
 function App() {
   const [darkMode, setDarkMode] = useState("light");
@@ -50,7 +50,12 @@ function App() {
 
   return (
     <>
-      <Router>
+    <Navbar title="TextUtils" mode={darkMode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
+      <div className="container my-3">
+      <TextForm showAlert={showAlert} heading="Enter the text to Analyse" mode={darkMode} />
+      </div>
+      {/* <Router>
       <Navbar title="TextUtils" mode={darkMode} toggleMode={toggleMode} />
       <Alert alert={alert} />
 
@@ -62,7 +67,7 @@ function App() {
           </Route>
       </Routes>
       </div>
-      </Router>
+      </Router> */}
     </>
   );
 }
